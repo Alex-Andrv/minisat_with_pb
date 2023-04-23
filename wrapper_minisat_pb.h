7be57821f479e4de94830dc5fb6440bec4098f09
alexanderandreev <sesh_1.0@niuitmo.ru>
2023-04-23 23:09:00 +0300

    add wrapper_minisat_pb.h

--- wrapper_minisat_pb.h
+++ wrapper_minisat_pb.h
@@ -11,39 +11,41 @@
 #include <thread>
 #include <vector>
 
-
-class WrappedMinisatSolver : public Minisat::Solver {
-public:
-    void addClause(std::vector<int> lits) {
-        Minisat::vec<Minisat::Lit> ps;
-        for (int lit : lits) {
-            ps.push(get_lit(lit));
+namespace Minisat {
+    class WrappedMinisatSolver : public Minisat::Solver {
+    public:
+        void addClause(std::vector<int> lits) {
+            Minisat::vec<Minisat::Lit> ps;
+            for (int lit: lits) {
+                ps.push(get_lit(lit));
+            }
+            addClause_(ps);
         }
-        addClause_(ps);
-    }
 
-    bool addLeqAssign(std::vector<int> lits, std::vector<int> coeff, int bound) {
-        Minisat::vec<Minisat::Term> ps;
-        assert(lits.size() == coeff.size());
-        for (int i = 0; i < static_cast<int>(lits.size()); i++) {
-            ps.push(Minisat::Term{coeff[i], get_lit(lits[i])});
+        bool addLeqAssign(std::vector<int> lits, std::vector<int> coeff, int bound) {
+            Minisat::vec<Minisat::Term> ps;
+            assert(lits.size() == coeff.size());
+            for (int i = 0; i < static_cast<int>(lits.size()); i++) {
+                ps.push(Minisat::Term{coeff[i], get_lit(lits[i])});
+            }
+            addLeqAssign_(ps, bound);
         }
-        addLeqAssign_(ps, bound);
-    }
 
-    bool addGeqAssign(std::vector<int> lits, std::vector<int> coeff, int bound) {
-        Minisat::vec<Minisat::Term> ps;
-        assert(lits.size() == coeff.size());
-        for (int i = 0; i < static_cast<int>(lits.size()); i++) {
-            ps.push(Minisat::Term{coeff[i], get_lit(lits[i])});
+        bool addGeqAssign(std::vector<int> lits, std::vector<int> coeff, int bound) {
+            Minisat::vec<Minisat::Term> ps;
+            assert(lits.size() == coeff.size());
+            for (int i = 0; i < static_cast<int>(lits.size()); i++) {
+                ps.push(Minisat::Term{coeff[i], get_lit(lits[i])});
+            }
+            addGeqAssign_(ps, bound);
         }
-        addGeqAssign_(ps, bound);
-    }
-private:
-    Minisat::Lit get_lit(int lit) {
-        int var = abs(lit) - 1;
-        while (var >= nVars())
-            newVar();
-        return (lit > 0) ? Minisat::mkLit(var) : ~Minisat::mkLit(var);
-    }
-};
+
+    private:
+        Minisat::Lit get_lit(int lit) {
+            int var = abs(lit) - 1;
+            while (var >= nVars())
+                newVar();
+            return (lit > 0) ? Minisat::mkLit(var) : ~Minisat::mkLit(var);
+        }
+    };
+}
